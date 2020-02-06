9d229238ff44e7570da3351f9b7688e3466a21a8
Guillermo Gonzalez <gonzaleztrevijano@gmail.com>
2020-02-06 16:06:56 +0100

    Piramide

--- Piramide/Program.cs
+++ Piramide/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Piramide
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Saludos, ¿Que figura deseas construir: \n1) Piramide \n2) Faldon Derecho \n3) Faldon Izquierdo \n4) Piramide Invertida");
+            Console.WriteLine("Saludos, ¿Que figura deseas construir?: \n1) Piramide \n2) Faldon Derecho \n3) Faldon Izquierdo \n4) Piramide Invertida");
             Console.Write("\r\n");
 
             string dato = Console.ReadLine();
@@ -17,18 +17,29 @@ namespace Piramide
                 case 1:
                     {
                         Console.WriteLine("Construyendo la Piramide...");
+                        int espacios = 8;
 
-                        for (int i = 0; i < 7; i++) //Este for genera los asteriscos en Vertical
-                        {                          
-                            for (int e = 0; e < i; e++)  //Este for genera los asteriscos en Horizontal al usar solo Write
-                            {                               
-                                Console.Write("*", e);                                 
+                        for (int i = 0; i < 8; i++) //Genera un espacio en horizontal
+                        {
+              
+                            Console.WriteLine("", i); 
+
+                            for (int f = 0; f < espacios ; f++) //Genera un espacio de 7 huecos, luego va decreciendo con "espacios--"
+                            {
+                                    Console.Write(" ", f);   
                             }
-                                for (int d = i; d < i; d++) //Este for va sumadando * hasta que iguala la cantidad de i, que es 7.
-                                {
-                                    Console.WriteLine();
-                                }                          
-                            Console.WriteLine("*", i);
+
+                            for (int e = 0; e < i; e++) //Genera un la piramide incrementandose de 1 en 1
+                            {
+                                Console.Write("*", e);
+                                
+                            }
+                            espacios--;
+
+                            for (int s = 1; s < i; s++) //Genera la piramide que se incrementa a partir del 2 asterisco del lado anterior
+                            {
+                                Console.Write("*", s);                              
+                            }
                         }
                     }
                     break;
@@ -37,55 +48,75 @@ namespace Piramide
                     {
                         Console.WriteLine("Construyendo el faldon derecho...");
 
-                        for (int i = 0; i < 7; i++)
+                        for (int i = 0; i < 7; i++) //Te genera 7 numeros
                         {
                             
-                            for (int e = 0; e < i; e++)
+                            for (int e = 0; e < i; e++) // Te genera 6 numeros
                             {                               
-                                Console.Write("*", e);                                                            
-                            }
-                                for (int d = i; d < i; d++)
-                                {
-                                 Console.WriteLine();
-                                }
-
-                            Console.WriteLine("*", i);                                                    
+                                Console.Write("*", e); //Te cambia los 6 numeros por asteriscos                   
+                            }                            
+                            Console.WriteLine("*", i);  //Cambia los numeros del primer for por Asteriscos 
                         }
-
                     }
                     break;
 
                 case 3:
                     {
-                        int numAsteriscos = 1; //Variable
-                        int numEspacios = 6; //Varible
+                        Console.WriteLine("Construyendo el faldon izquierdo...");
 
-                        for (int i = 0; i < 6; i++) //Te genera 6 lineas vacias en horizontal
+                        int numAsteriscos = 1; // Variable
+                        int numEspacios = 6; // Varible
+
+                        for (int i = 0; i < 6; i++) // Te genera 6 lineas vacias
                         {
-                            string linea = ""; //Variable vacia
+                            string linea = ""; // Variable vacia
 
-                            for (int k = 0; k < numEspacios; k++) //Te genera 5 espacios
+                            for (int k = 0; k < numEspacios; k++) // Te genera 5 espacios que se van incremendanto de uno en uno
                             {
-                                linea = linea + " "; 
+                                linea = linea + " "; // Te genera espacios en la variable linea
                             }
 
-                            for (int j = 0; j < numAsteriscos; j++) //Te genera 1 linea y la convierte en asterisco
+                            for (int j = 0; j < numAsteriscos; j++) // Te genera los asteriscos incrementandose de uno en uno
                             {
-                                linea = linea + "*";
+                                linea = linea + "*";    // Te añade asteriscos a la variable linea
                             }
 
-                            Console.Write(linea); 
-                            Console.WriteLine();
-                            numAsteriscos++;
-                            numEspacios--;
+                            Console.Write(linea);  // Te pinta los datos de los for
+                            Console.WriteLine();    //Deja un espacio entre los asteriscos
+                            numAsteriscos++;    //  Incrementa el número de asteriscos gradualmente
+                            numEspacios--;  //  Reduce el númro de espacios gradualmente
                         }
                     }
                     break;
 
+                case 4:
+                    {
+                        Console.WriteLine("Construyendo la Piramide inversa...");
+
+                        for (int i = 0; i < 7; i++)
+                        {
+
+                            Console.WriteLine("", i);
+
+                            for (int f = 0; f < i; f++) 
+                            {
+                                Console.Write(" ", f);
+                            }
+
+                            for (int e = 7; e > i; e--) 
+                            {
+                                Console.Write("*", e);
+                            }
+
+                            for (int s = 6; s > i; s--) 
+                            {
+                                Console.Write("*", s);
+                            }
+                        }
+                    }
+                    break;
             }
-
             Console.Write("\r\n");
-
         }
     }
 }
